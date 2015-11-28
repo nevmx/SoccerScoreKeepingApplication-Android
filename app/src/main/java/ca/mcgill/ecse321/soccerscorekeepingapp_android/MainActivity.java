16e438d83a593d0c1ce24ebc9ea011b56b54273f
nevmx <maxsnev@gmail.com>
2015-11-28 17:54:38 -0500

    Authentication enabled for manager and input modes

--- app/src/main/java/ca/mcgill/ecse321/soccerscorekeepingapp_android/MainActivity.java
+++ app/src/main/java/ca/mcgill/ecse321/soccerscorekeepingapp_android/MainActivity.java
@@ -1,15 +1,21 @@
 package ca.mcgill.ecse321.soccerscorekeepingapp_android;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import ca.mcgill.ecse321.soccerscorekeeping.admin.authentication;
 import ca.mcgill.ecse321.soccerscorekeeping.admin.managerTools;
 
 public class MainActivity extends AppCompatActivity {
@@ -71,16 +77,57 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /* Called when batch input mode button is pressed */
-    public void openCreateMatchPageForBatch(View view) {
+    /* Called when input mode selected */
+    public void authenticateScoreKeeper(final View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        builder.setTitle("Enter scorekeeper password")
+                .setView(input)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (authentication.authenticateScoreKeeper(input.getText().toString().toCharArray())) {
+
+                            Toast.makeText(MainActivity.this, "Scorekeeper Mode Active", Toast.LENGTH_LONG).show();
+
+                            switch (view.getId()) {
+                                case R.id.liveInputButton:
+                                    openCreateMatchPageForLive();
+                                    break;
+                                case R.id.batchInputButton:
+                                    openCreateMatchPageForBatch();
+                                    break;
+                                default:
+                                    Toast.makeText(MainActivity.this, "Unexpected Error", Toast.LENGTH_LONG).show();
+                                    break;
+                            }
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Wrong password. Please try again.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    private void openCreateMatchPageForBatch() {
         Intent intent = new Intent(this, CreateMatchActivity.class);
         String mode = "batch";
         intent.putExtra(MODE, mode);
         startActivity(intent);
     }
 
-    /* Called when live input mode button is pressed */
-    public void openCreateMatchPageForLive(View view) {
+    private void openCreateMatchPageForLive() {
         Intent intent = new Intent(this, CreateMatchActivity.class);
         String mode = "live";
         intent.putExtra(MODE, mode);
@@ -88,7 +135,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /* Called when manager button is pressed */
-    public void openManagerActivity(View view) {
+    public void authenticateManager(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        builder.setTitle("Enter manager password")
+                .setView(input)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (authentication.authenticate(input.getText().toString().toCharArray())) {
+                            Toast.makeText(MainActivity.this, "Manager Mode Active", Toast.LENGTH_LONG).show();
+                            openManagerActivity();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Wrong password. Please try again.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    private void openManagerActivity() {
         Intent intent = new Intent(this, ManagerActivity.class);
         startActivity(intent);
     }
